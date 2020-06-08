92b495fdaef15543355e8bf64257d87e7e441efa
Justin Field <justin.field@armory.io>
2020-06-08 16:21:20 -0700

    chore: port MetricsConfiguration from Armory Commons

---
+++ common/src/main/java/io/armory/plugin/metrics/model/ArmoryEnvironmentMetadata.java
@@ -0,0 +1,16 @@
+package io.armory.plugin.metrics.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ArmoryEnvironmentMetadata {
+    private final String applicationName;
+    private final String armoryAppVersion;
+    private final String ossAppVersion;
+    private final String spinnakerRelease;
+    private final String customerEnvId;
+    private final String customerEnvName;
+    private final String customerName;
+}
