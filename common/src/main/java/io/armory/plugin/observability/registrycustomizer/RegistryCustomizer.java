b2c08aaa305f8e4e3d08e2e68dac1069bef62a55
Justin Field <justin.field@armory.io>
2020-06-15 15:53:02 -0700

    fix: call the doCustomize on the customizers

--- common/src/main/java/io/armory/plugin/observability/registrycustomizer/RegistryCustomizer.java
+++ common/src/main/java/io/armory/plugin/observability/registrycustomizer/RegistryCustomizer.java
@@ -9,20 +9,22 @@ public abstract class RegistryCustomizer implements MeterRegistryCustomizer<Mete
 
   @Override
   public void customize(MeterRegistry registry) {
-    var registryClass = registry.getClass().toString();
+    var registryClass = registry.getClass().getSimpleName();
     try {
       // use reflection to get the actual impl
       // https://github.com/Netflix/spectator/blob/cd320b624d9f28ae67cd285b363afabfcd070006/spectator-reg-micrometer/src/main/java/com/netflix/spectator/micrometer/MicrometerRegistry.java#L46
       // We try catch because 1 day this will break and reflection is generally bad. ¯\(°_o)/¯
       var registryImpl = registry.getClass().getDeclaredField("impl");
-      registryClass = registryImpl.getClass().toString();
+      registryClass = registryImpl.getClass().getSimpleName();
     } catch (NoSuchFieldException e) {
       // DO NOTHING
     }
     log.info(
         "{} is preparing to customize {} registry class if enabled",
-        this.getClass(),
+        this.getClass().getSimpleName(),
         registryClass);
+
+    doCustomize(registry);
   }
 
   abstract void doCustomize(MeterRegistry registry);
