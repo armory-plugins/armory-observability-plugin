d5a72f21c5bb5918a19849de75bfcbc2afd95ec7
manuelaguir <manuel.aguirre@armory.io>
2022-09-24 20:09:39 -0500

    disable bean

--- common/src/main/java/io/armory/plugin/observability/datadog/DataDogRegistrySupplier.java
+++ common/src/main/java/io/armory/plugin/observability/datadog/DataDogRegistrySupplier.java
@@ -4,22 +4,26 @@ import io.armory.plugin.observability.model.PluginConfig;
 import io.armory.plugin.observability.model.PluginMetricsDatadogConfig;
 import io.armory.plugin.observability.registry.RegistryConfigWrapper;
 import io.micrometer.core.instrument.Clock;
+import io.micrometer.core.ipc.http.HttpUrlConnectionSender;
 import io.micrometer.datadog.DatadogMeterRegistry;
 import org.jetbrains.annotations.NotNull;
+import io.micrometer.core.ipc.http.HttpSender;
 
+import java.time.Duration;
 import java.util.function.Supplier;
 
 
 public class DataDogRegistrySupplier implements Supplier<RegistryConfigWrapper> {
 
     private final PluginMetricsDatadogConfig datadogConfig;
-    private final DatadogMeterRegistry collectorRegistry;
+    //private final DatadogMeterRegistry collectorRegistry;
+    protected HttpUrlConnectionSender sender;
     private final Clock clock;
 
-    public DataDogRegistrySupplier (@NotNull PluginConfig pluginConfig, DatadogMeterRegistry collectorRegistry, Clock clock) {
+    public DataDogRegistrySupplier (@NotNull PluginConfig pluginConfig, Clock clock) {
         datadogConfig = pluginConfig.getMetrics().getDatadog();
-        this.collectorRegistry = collectorRegistry;
         this.clock = clock;
+        this.sender =  new HttpUrlConnectionSender(Duration.ofSeconds(datadogConfig.getConnectDurationSeconds()),Duration.ofSeconds(datadogConfig.getReadDurationSeconds()));
     }
 
     @Override
@@ -28,7 +32,8 @@ public class DataDogRegistrySupplier implements Supplier<RegistryConfigWrapper> 
         return null;
       }
       var config = new DataDogRegistryConfig(datadogConfig);
-      var registry = DatadogMeterRegistry.builder(config).build();
+      var registry = DatadogMeterRegistry.builder(config).httpClient(sender).build();
+
         return RegistryConfigWrapper.builder()
                 .meterRegistry(registry)
                 .meterRegistryConfig(datadogConfig.getMeterRegistryConfig())
