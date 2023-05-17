e836d3ef53d6addcab336667de8c3a668640365a
SusmithaGundu <susmitha.gundu@opsmx.io>
2023-05-17 13:54:25 +0530

    compiled with java17

--- common/src/main/java/io/armory/plugin/observability/model/SecurityConfig.java
+++ common/src/main/java/io/armory/plugin/observability/model/SecurityConfig.java
@@ -1,33 +1,37 @@
 package io.armory.plugin.observability.model;
 
-import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 //To avoid collision with other WebSecurityConfigurerAdapters
 @Order(Ordered.HIGHEST_PRECEDENCE + 27)
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
+public class SecurityConfig implements WebMvcConfigurer {
     private final PluginConfig pluginConfig;
 
     public SecurityConfig(PluginConfig pluginConfig) {
         this.pluginConfig = pluginConfig;
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-       if (pluginConfig.getMetrics().getPrometheus().isEnabled()) {
-           http.requestMatcher(EndpointRequest.to("aop-prometheus")).authorizeRequests((requests) ->
-                   requests.anyRequest().permitAll());
-       } else {
-           http.requestMatcher(EndpointRequest.to("aop-prometheus")).authorizeRequests((requests) ->
-                   requests.anyRequest().denyAll());
-       }
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        if (pluginConfig.getMetrics().getPrometheus().isEnabled()) {
+            http.authorizeHttpRequests()
+                    .requestMatchers("aop-prometheus")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated();
+            return http.build();
+        } else {
+            http.authorizeHttpRequests((authz) -> authz.requestMatchers("aop-prometheus").denyAll());
+            return http.build();
+        }
     }
 }
